d20a99a16de089c7987c1088a44e5d43697d2c45
xuxinlai2002 <xuxinlai2002@gmail.com>
2020-08-04 13:37:48 +0800

    add galt script

---
+++ galt/galtPeerRun.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+nohup ./galt --rpc --rpcapi db,eth,net,web3,personal \
+--cache=2048 --port 25000 --rpcport 8025 --rpcaddr "0.0.0.0" --rpccorsdomain "*" \
+--networkid 55663  --gcmode archive >output 2>&1  &
