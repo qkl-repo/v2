ebdf2b9174731d1b9196387d193bcc1a0f20a69d
xuxinlai <xuxinlai2002@gmail.com>
2020-08-05 01:30:37 +0800

    add for the unlock

--- galt/galtPeerRun.sh
+++ galt/galtPeerRun.sh
@@ -2,4 +2,4 @@
 
 nohup ./galt --rpc --rpcapi db,eth,net,web3,personal \
 --cache=2048 --port 25000 --rpcport 8025 --rpcaddr "0.0.0.0" --rpccorsdomain "*" \
---networkid 55663  --gcmode archive >output 2>&1  &
+--allow-insecure-unlock --networkid 55663  --gcmode archive >output 2>&1  &
