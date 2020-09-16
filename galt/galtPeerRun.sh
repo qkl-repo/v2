ac6a3b3533e7eb9101ab06844fdfc36c78049bca
xuxinlai <xuxinlai2002@gmail.com>
2020-09-17 00:55:27 +0800

    add

--- galt/galtPeerRun.sh
+++ galt/galtPeerRun.sh
@@ -2,4 +2,5 @@
 
 nohup ./galt --rpc --rpcapi db,eth,net,web3,personal \
 --cache=2048 --port 25000 --rpcport 8025 --rpcaddr "0.0.0.0" --rpccorsdomain "*" \
+--nodiscover \
 --datadir data --allow-insecure-unlock --networkid 55663  --gcmode archive >output 2>&1  &
