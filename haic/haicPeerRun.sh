27915cbb3ffe79b0af08e0cb61c2898eec1487ea
xxl <xxl@local.com>
2020-10-19 14:26:24 +0800

    add nodiscover

--- haic/haicPeerRun.sh
+++ haic/haicPeerRun.sh
@@ -2,4 +2,5 @@
 
 nohup ./haic --rpc --rpcapi db,eth,net,web3,personal \
 --cache=2048 --port 15000 --rpcport 8022 --rpcaddr "0.0.0.0" --rpccorsdomain "*" \
+--nodiscover \
 --datadir data --allow-insecure-unlock --networkid 55662  --gcmode archive >output 2>&1 &
